1e6b3f3a60e1ba01fb18a792c304ab4d9dc7e39e
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-02-14 22:12:28 +0200

    Added a function to move a model between cpu and gpu

--- src/FluxExtra.jl
+++ src/FluxExtra.jl
@@ -3,7 +3,8 @@ module FluxExtra
 using Flux, CUDA
 import CUDA.CuArray
 include("layers.jl")
+include("utilities.jl")
 
-export Parallel, Catenation, Decatenation, Upscaling, Addition, Activation, Identity
+export Parallel, Catenation, Decatenation, Upscaling, Addition, Activation, Identity, move
 
 end
