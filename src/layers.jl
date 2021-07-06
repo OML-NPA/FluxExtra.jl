a498adc28570fd5af3b9924ec43f6b85035a9c98
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-06 19:37:27 +0300

    Added Matrix and error handling for Split

--- src/layers.jl
+++ src/layers.jl
@@ -11,7 +11,26 @@ struct Split
     dim::Int64
 end
 function Split_func(x::T,outputs::Int64, 
+        dim::Int64) where T<:AbstractArray{<:AbstractFloat,2}
+    if dim>2
+        throw(BoundsError(x,dim))
+    end
+    step_var = Int64(size(x, dim) / outputs)
+    f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
+    vals = ntuple(i -> i, outputs)
+    inds_vec = map(f,vals)
+    if dim == 1
+        x_out = map(inds -> x[inds,:],inds_vec)
+    else # dim == 2
+        x_out = map(inds -> x[:,inds],inds_vec)
+    end
+    return x_out
+end
+function Split_func(x::T,outputs::Int64, 
         dim::Int64) where T<:AbstractArray{<:AbstractFloat,4}
+    if dim>2
+        throw(BoundsError(x,dim))
+    end
     step_var = Int64(size(x, dim) / outputs)
     f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
     vals = ntuple(i -> i, outputs)
@@ -20,7 +39,7 @@ function Split_func(x::T,outputs::Int64,
         x_out = map(inds -> x[inds,:,:,:],inds_vec)
     elseif dim == 2
         x_out = map(inds -> x[:,inds,:,:],inds_vec)
-    else
+    else # dim == 3
         x_out = map(inds -> x[:,:,inds,:],inds_vec)
     end
     return x_out
