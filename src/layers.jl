ca47e07d377a6bbc2e74ac13473ce780038a2c32
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-06 22:19:03 +0300

    Added Flatten() and support for Dense

--- src/layers.jl
+++ src/layers.jl
@@ -3,7 +3,32 @@
 struct Join
     dim::Int64
 end
-(m::Join)(x::NTuple{N,T}) where {T,N} = cat(x...,dims=m.dim)::T
+
+function cat_dispatcher(x::NTuple{N,T},dim::Int64) where {N,T<:AbstractMatrix}
+    if dim==1
+        return vcat(x...)
+    else
+        msg::String = string("Only dimensions 1 is supported. Given ",dim,".")
+        err::DimensionMismatch = DimensionMismatch(msg)
+        throw(err)
+    end
+end
+
+function cat_dispatcher(x::NTuple{N,T},dim::Int64) where {N,Y,T<:AbstractArray{Y,4}}
+    if dim==1
+        return vcat(x...)
+    elseif dim==2
+        return hcat(x...)
+    elseif dim==3
+        return cat(x...,dims=Val(3))
+    else
+        msg::String = string("Only dimensions 1,2 and 3 are supported. Given ",dim,".")
+        err::DimensionMismatch = DimensionMismatch(msg)
+        throw(err)
+    end
+end
+
+(m::Join)(x::NTuple{N,T}) where {T<:AbstractArray,N} = cat_dispatcher(x,m.dim)
 
 # Split layer
 struct Split
@@ -12,24 +37,20 @@ struct Split
 end
 function Split_func(x::T,outputs::Int64, 
         dim::Int64) where T<:AbstractArray{<:AbstractFloat,2}
-    if dim>2
-        throw(BoundsError(x,dim))
+    if dim!=1
+        throw(DimensionMismatch("Dimension should be 1."))
     end
     step_var = Int64(size(x, dim) / outputs)
     f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
     vals = ntuple(i -> i, outputs)
     inds_vec = map(f,vals)
-    if dim == 1
-        x_out = map(inds -> x[inds,:],inds_vec)
-    else # dim == 2
-        x_out = map(inds -> x[:,inds],inds_vec)
-    end
+    x_out = map(inds -> x[inds,:],inds_vec)
     return x_out
 end
 function Split_func(x::T,outputs::Int64, 
         dim::Int64) where T<:AbstractArray{<:AbstractFloat,4}
-    if dim>2
-        throw(BoundsError(x,dim))
+    if dim>3
+        throw(DimensionMismatch("Dimension should be 1, 2 or 3."))
     end
     step_var = Int64(size(x, dim) / outputs)
     f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
@@ -55,6 +76,11 @@ struct Activation
     f::Function
 end
 (m::Activation)(x) = m.f.(x)
+
+# Flatten
+struct Flatten
+end
+(m::Flatten)(x) = Flux.flatten(x)
 
 # Identity layer
 struct Identity
