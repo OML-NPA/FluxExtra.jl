f01418d2d62a4d3f6182a52590f69c3dab03dd01
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-07 12:28:22 +0300

    Improved syntax and Split_func

--- src/layers.jl
+++ src/layers.jl
@@ -1,56 +1,78 @@
 
 # Join layer
 struct Join{D}
+    dim::Int64
     function Join(dim)
         if dim>3
             throw(DimensionMismatch("Dimension should be 1, 2 or 3."))
         end
-        new{dim}()
+        new{dim}(dim)
+    end
+    function Join(;dim)
+        if dim>3
+            throw(DimensionMismatch("Dimension should be 1, 2 or 3."))
+        end
+        new{dim}(dim)
     end
 end
-(m::Join{D})(x::NTuple{N,T}) where {D,N,T<:AbstractArray} = cat(x...,dims = Val(D))
+(m::Join{D})(x::NTuple{N,AbstractArray}) where {D,N} = cat(x...,dims = Val(D))
 
+function Base.show(io::IO, l::Join)
+    print(io, "Join(", "dim = ",l.dim, ")")
+end
 
 # Split layer
-struct Split{D}
+struct Split{O,D}
+    outputs::Int64
     dim::Int64
     function Split(outputs,dim)
         if dim>3
             throw(DimensionMismatch("Dimension should be 1, 2 or 3."))
         end
-        new{outputs}(dim)
+        new{outputs,dim}(outputs,dim)
+    end
+    function Split(outputs;dim)
+        if dim>3
+            throw(DimensionMismatch("Dimension should be 1, 2 or 3."))
+        end
+        new{outputs,dim}(outputs,dim)
     end
 end
-function Split_func(x::T,m::Split{D}) where {D,T<:AbstractArray{<:AbstractFloat,2}}
-    dim = m.dim
-    if dim!=1
+
+function Split_func(x::T,m::Split{O,D}) where {O,D,T<:AbstractArray{<:AbstractFloat,2}}
+    if D!=1
         throw(DimensionMismatch("Dimension should be 1."))
     end
-    step_var = Int64(size(x, dim) / D)
+    step_var = Int64(size(x, D) / O)
     f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
-    vals = ntuple(i -> i, D)
+    vals = ntuple(i -> i, O)
     inds_vec = map(f,vals)
     x_out = map(inds -> x[inds,:],inds_vec)
     return x_out
 end
-function Split_func(x::T,m::Split{D}) where {D,T<:AbstractArray{<:AbstractFloat,4}}
-    dim = m.dim
-    step_var = Int64(size(x, dim) / D)
-    f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
-    vals = ntuple(i -> i, D)
-    inds_vec = map(f,vals)
-    if dim == 1
-        x_out = map(inds -> x[inds,:,:,:],inds_vec)
-        return x_out
-    elseif dim == 2
-        x_out = map(inds -> x[:,inds,:,:],inds_vec)
-        return x_out
-    else # dim == 3
-        x_out = map(inds -> x[:,:,inds,:],inds_vec)
-        return x_out
-    end
+
+function get_part(x::T,inds::NTuple{O,UnitRange{Int64}},d::Type{Val{1}}) where {O,T<:AbstractArray{<:AbstractFloat,4}}
+    x_out = map(inds -> x[inds,:,:,:],inds)
 end
-(m::Split{D})(x::T) where {D,T<:AbstractArray} = Split_func(x,m)
+function get_part(x::T,inds::NTuple{O,UnitRange{Int64}},d::Type{Val{2}}) where {O,T<:AbstractArray{<:AbstractFloat,4}}
+    x_out = map(inds -> x[:,inds,:,:],inds)
+end
+function get_part(x::T,inds::NTuple{O,UnitRange{Int64}},d::Type{Val{3}}) where {O,T<:AbstractArray{<:AbstractFloat,4}}
+    x_out = map(inds -> x[:,:,inds,:],inds)
+end
+function Split_func(x::T,m::Split{O,D}) where {O,D,T<:AbstractArray{<:AbstractFloat,4}}
+    step_var = Int64(size(x, D) / O)
+    f = i::Int64 -> (1+(i-1)*step_var):(i)*step_var
+    vals = ntuple(i -> i, O)
+    inds_tuple = map(f,vals)
+    x_out = get_part(x,inds_tuple,Val{D})
+    return x_out
+end
+(m::Split{O,D})(x::T) where {O,D,T<:AbstractArray} = Split_func(x,m)
+
+function Base.show(io::IO, l::Split)
+    print(io, "Split(", l.outputs,", dim = ",l.dim, ")")
+end
 
 # Makes Parallel layer type stable when used after Split
 (m::Parallel)(xs::NTuple{N,AbstractArray}) where N = map((f,x) -> f(x), m.layers,xs)
@@ -62,9 +84,14 @@ end
 
 # Activation layer
 struct Activation{F}
-    Activation(f) = new{f}()
+    f
+    Activation(f) = new{f}(f)
 end
 (m::Activation{F})(x) where F = F.(x)
+
+function Base.show(io::IO, l::Activation)
+    print(io, "Activation(",l.f, ")")
+end
 
 # Flatten layer
 struct Flatten 
