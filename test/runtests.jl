5ed3eeb3f8af3c2dcf8fcb684ed1e4eda42126e9
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-07-09 13:18:49 +0300

    Improved tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,179 +18,254 @@ function test_training(model,x,y)
     if losses[1]==losses[2]
         error("Parameters not updating.")
     end
-    return nothing
+    return true
 end
 
-function test(model,x,y)
-    @inferred model(x)
-    test_training(model,x,y)
-    @inferred gpu(model)(gpu(x))
-    test_training(gpu(model),gpu(x),gpu(y))
+function test_model(model,x,y) 
+    test_exp = quote
+        @test begin
+            @inferred $model($x) 
+            true 
+        end
+        @test test_training($model,$x,$y)
+        @test begin 
+            @inferred gpu($model)(gpu($x)) 
+            true 
+        end
+        @test test_training(gpu($model),gpu($x),gpu($y))
+    end
+    return test_exp
 end
 
-#---Convolution-----------------------------------------------------------
 
-test_layer = Conv((3, 3), 1=>2,pad=SamePad())
-test_layer2 = Conv((3, 3), 1=>2,pad=SamePad())
+#---Tests begin-------------------------------------------------------------
 
-# Test Join layer
-x = ones(Float32,6,6,1,1)
-y = ones(Float32,12,6,2,1)
-model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(1))
-test(model,x,y)
+@testset verbose=true "Conv as input" begin
+    test_layer = Conv((3, 3), 1=>2,pad=SamePad())
+    test_layer2 = Conv((3, 3), 1=>2,pad=SamePad())
 
-x = ones(Float32,6,6,1,1)
-y = ones(Float32,6,12,2,1)
-model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(2))
-test(model,x,y)
+    @testset "Join" begin
+        @testset "Join(1)" begin
+            x = ones(Float32,6,6,1,1)
+            y = ones(Float32,12,6,2,1)
+            model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(1))
+            eval(test_model(model,x,y))
+        end
+        
+        @testset "Join(2)" begin
+            x = ones(Float32,6,6,1,1)
+            y = ones(Float32,6,12,2,1)
+            model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(2))
+            eval(test_model(model,x,y))
+        end
+        
+        @testset "Join(dum = 3)" begin
+            x = ones(Float32,6,6,1,1)
+            y = ones(Float32,6,6,4,1)
+            model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(dim = 3))
+            eval(test_model(model,x,y))
+        end
+        
+        @testset "Join errors" begin
+            @test try
+                    Join(4)
+                catch e
+                    if !(e isa DimensionMismatch)
+                        @error "Wrong error returned."
+                        return e
+                    else
+                        true
+                    end
+                end
 
-x = ones(Float32,6,6,1,1)
-y = ones(Float32,6,6,4,1)
-model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(dim = 3))
-test(model,x,y)
+            @test try
+                    Join(dim = 4)
+                catch e
+                    if !(e isa DimensionMismatch)
+                        @error "Wrong error returned."
+                        return e
+                    else
+                        true
+                    end
+                end
+        end
+        
+        @testset "Printing" begin
+            @test begin 
+                Base.show(IOBuffer(),Join(1))
+                true
+            end
+        end
+    end
 
-try
-    Join(4)
-catch e
-    if !(e isa DimensionMismatch)
-        error("Wrong error returned.")
+    @testset "Split" begin
+        @testset "Split(1)" begin
+            x = ones(Float32,6,6,1,1)
+            y = ones(Float32,6,6,2,1)
+            model = Chain(Split(2,1),Parallel(tuple,(test_layer,test_layer)),Join(1))
+            eval(test_model(model,x,y))
+        end
+        
+        @testset "Split(2)" begin
+            x = ones(Float32,6,6,1,1)
+            y = ones(Float32,6,6,2,1)
+            model = Chain(Split(2,2),Parallel(tuple,(test_layer,test_layer)),Join(2))
+            eval(test_model(model,x,y))
+        end
+
+        @testset "Split(dim = 3)" begin
+            x = ones(Float32,6,6,2,1)
+            y = ones(Float32,6,6,4,1)
+            model = Chain(Split(2,3),Parallel(tuple,(test_layer,test_layer)),Join(3))
+            eval(test_model(model,x,y))
+        end
+
+        @testset "Split errors" begin
+            @test try
+                    Split(2,4)
+                catch e
+                    if !(e isa DimensionMismatch)
+                        @error "Wrong error returned."
+                        return e
+                    else
+                        true
+                    end
+                end
+
+            @test try
+                    Split(2,dim = 4)
+                catch e
+                    if !(e isa DimensionMismatch)
+                        @error "Wrong error returned."
+                        return e
+                    else
+                        true
+                    end
+                end
+
+            @test try
+                    Split(1,1)
+                catch e
+                    if !(e isa DomainError)
+                        @error "Wrong error returned."
+                        return e
+                    else
+                        true
+                    end
+                end
+
+            @test try
+                    Split(1,dim = 1)
+                catch e
+                    if !(e isa DomainError)
+                        @error "Wrong error returned."
+                        return e
+                    else
+                        true
+                    end
+                end
+        end
+
+        @testset "Printing" begin
+            @test begin
+                Base.show(IOBuffer(),Split(2,3)) 
+                true
+            end
+        end
+    end
+
+    @testset "Addition" begin
+        x = ones(Float32,6,6,1,1)
+        y = ones(Float32,6,6,1,1)
+        model = Chain(Parallel(tuple,(test_layer,test_layer)),Addition())
+        eval(test_model(model,x,y))
+    end
+
+    @testset "Activation" begin
+        x = ones(Float32,4,4,1,1)
+        y = ones(Float32,4,4,2,1)
+        model = Chain(test_layer,Activation(tanh))
+        eval(test_model(model,x,y))
+
+        @testset "Printing" begin
+            @test begin 
+                Base.show(IOBuffer(),Activation(tanh))
+                true
+            end
+        end
+    end
+
+    @testset "Flatten" begin
+        x = ones(Float32,4,4,1,1)
+        y = ones(Float32,32,1)
+        model = Chain(test_layer,Flatten())
+        eval(test_model(model,x,y))
+    end
+
+    @testset "Identity" begin
+        x = ones(Float32,4,4,1,1)
+        y = ones(Float32,4,4,3,1)
+        model = Chain(Parallel(tuple,(test_layer,Identity())),Join(3))
+        eval(test_model(model,x,y))
     end
 end
 
-try
-    Join(dim = 4)
-catch e
-    if !(e isa DimensionMismatch)
-        error("Wrong error returned.")
+
+@testset verbose=true "Dense as input" begin
+    test_layer = Dense(2,3)
+    test_layer2 = Dense(2,3)
+
+    @testset "Join" begin
+        @testset "Join(1)" begin
+        x = ones(Float32,2,1)
+        y = ones(Float32,6,1)
+        model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(1))
+        eval(test_model(model,x,y))
+    end
+
+    @testset "Split" begin
+        @testset "Split(1)" begin
+            x = ones(Float32,4,1)
+            y = ones(Float32,6,1)
+            model = Chain(Split(2,1),Parallel(tuple,(test_layer,test_layer)),Join(1))
+            eval(test_model(model,x,y))
+        end
+
+        @testset "Split errors" begin
+            x = ones(Float32,2,1)
+            layer = Split(2,2)
+            @test try
+                    layer(x)
+                catch e
+                    if !(e isa DimensionMismatch)
+                        error("Wrong error returned.")
+                        return e
+                    else
+                        true
+                    end
+                end
+            end
+        end
+    end
+
+    @testset "Addition" begin
+        x = ones(Float32,4,1)
+        y = ones(Float32,3,1)
+        model = Chain(Split(2,1),Parallel(tuple,(test_layer,test_layer)),Addition())
+        eval(test_model(model,x,y))
+    end
+
+    @testset "Activation" begin
+        x = ones(Float32,2,1)
+        y = ones(Float32,3,1)
+        model = Chain(test_layer,Activation(tanh))
+        eval(test_model(model,x,y))
+    end
+
+    @testset "Identity" begin
+        x = ones(Float32,2,1)
+        y = ones(Float32,5,1)
+        model = Chain(Parallel(tuple,(test_layer,Identity())),Join(1))
+        eval(test_model(model,x,y))
     end
 end
-
-Base.show(IOBuffer(),Join(1))
-
-
-# Test Split layer
-x = ones(Float32,6,6,1,1)
-y = ones(Float32,6,6,2,1)
-model = Chain(Split(2,dim = 1),Parallel(tuple,(test_layer,test_layer)),Join(1))
-test(model,x,y)
-
-x = ones(Float32,6,6,1,1)
-y = ones(Float32,6,6,2,1)
-model = Chain(Split(2,2),Parallel(tuple,(test_layer,test_layer)),Join(2))
-test(model,x,y)
-
-x = ones(Float32,6,6,2,1)
-y = ones(Float32,6,6,4,1)
-model = Chain(Split(2,3),Parallel(tuple,(test_layer,test_layer)),Join(3))
-test(model,x,y)
-
-try
-    Split(2,4)
-catch e
-    if !(e isa DimensionMismatch)
-        error("Wrong error returned.")
-    end
-end
-
-try
-    Split(2,dim = 4)
-catch e
-    if !(e isa DimensionMismatch)
-        error("Wrong error returned.")
-    end
-end
-
-try
-    Split(1,1)
-catch e
-    if !(e isa DomainError)
-        error("Wrong error returned.")
-    end
-end
-
-try
-    Split(1,dim = 1)
-catch e
-    if !(e isa DomainError)
-        error("Wrong error returned.")
-    end
-end
-
-Base.show(IOBuffer(),Split(2,3))
-
-
-# Test Addition layer
-x = ones(Float32,6,6,2,1)
-y = ones(Float32,6,6,1,1)
-model = Chain(Split(2,3),Parallel(tuple,(test_layer,test_layer)),Addition())
-test(model,x,y)
-
-
-# Test Activation layer
-x = ones(Float32,4,4,1,1)
-y = ones(Float32,4,4,2,1)
-model = Chain(test_layer,Activation(tanh))
-test(model,x,y)
-
-# Test Flatten layer
-x = ones(Float32,4,4,1,1)
-y = ones(Float32,32,1)
-model = Chain(test_layer,Flatten())
-test(model,x,y)
-
-Base.show(IOBuffer(),Activation(tanh))
-
-# Test Identity layer
-x = ones(Float32,4,4,1,1)
-y = ones(Float32,4,4,3,1)
-model = Chain(Parallel(tuple,(test_layer,Identity())),Join(3))
-test(model,x,y)
-
-#---Dense---------------------------------------------------------------
-
-test_layer = Dense(2,3)
-test_layer2 = Dense(2,3)
-
-# Test Join layer
-x = ones(Float32,2,1)
-y = ones(Float32,6,1)
-model = Chain(Parallel(tuple,(test_layer,test_layer2)),Join(1))
-test(model,x,y)
-
-
-# Test Split layer
-x = ones(Float32,4,1)
-y = ones(Float32,6,1)
-model = Chain(Split(2,1),Parallel(tuple,(test_layer,test_layer)),Join(1))
-test(model,x,y)
-
-x = ones(Float32,2,1)
-layer = Split(2,2)
-try
-    layer(x)
-catch e
-    if !(e isa DimensionMismatch)
-        error("Wrong error returned.")
-    end
-end
-
-
-# Test Addition layer
-x = ones(Float32,4,1)
-y = ones(Float32,3,1)
-model = Chain(Split(2,1),Parallel(tuple,(test_layer,test_layer)),Addition())
-test(model,x,y)
-
-
-# Test Activation layer
-x = ones(Float32,2,1)
-y = ones(Float32,3,1)
-model = Chain(test_layer,Activation(tanh))
-test(model,x,y)
-
-
-# Test Identity layer
-x = ones(Float32,2,1)
-y = ones(Float32,5,1)
-model = Chain(Parallel(tuple,(test_layer,Identity())),Join(1))
-test(model,x,y)
